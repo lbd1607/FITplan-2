b63e34556cc6f7b56361fc6a26654c87b0925e01
lbd1607 <35612977+lbd1607@users.noreply.github.com>
2021-04-29 01:03:28 -0500

    Finished fixing dashboard grid

--- fitplan/app/pages/index.tsx
+++ fitplan/app/pages/index.tsx
@@ -52,62 +52,53 @@ const Home: BlitzPage = () => {
           <UserInfo />
         </Suspense>
 
-        <div className="grid gap-4 grid-cols-12 ">
-          {/* Column 1 */}
-          <div className="col-span-7">
-            <div className="card-container-parent">
-              {/* Current Workout */}
-              <div className="dash-container ml-20">
-                <h1 className="m-3">
-                  Current Workout
-                  <Link href="/">
-                    {/* In Link, wrap icon in span to avoid ref error */}
-                    <span>
-                      <FontAwesomeIcon
-                        icon="chevron-right"
-                        size="sm"
-                        className="chevronicon float-right m-1"
-                      />
-                    </span>
-                  </Link>
-                </h1>
-                <div className="m-4">No workouts to show ...</div>
+        <div className="card-container-parent">
+          <div className="flex-1 mx-14 my-0 mb-8 p-6 ">
+            <div className="grid grid-cols-12 grid-rows-4 gap-4 grid-flow-row grid-flow-col h-full">
+              <div className="col-start-1 col-span-7 row-start-1 row-span-4 dash-card">
+                <div>
+                  <h1 className="m-3">
+                    Current Workout
+                    <Link href="/">
+                      <span>
+                        <FontAwesomeIcon
+                          icon="chevron-right"
+                          size="sm"
+                          className="chevronicon float-right m-1"
+                        />
+                      </span>
+                    </Link>
+                  </h1>
+                  <div className="m-4">No workouts to show ...</div>
+                </div>
               </div>
-            </div>
-          </div>
-          {/* Column 2 */}
-          <div className="card-container-parent col-span-5 ">
-            <div className="dash-container mr-20  ">
-              <div className="grid grid-rows-2 h-full gap-6 divide-y">
-                {/* Workouts */}
-                <div className="row-span-1 overflow-y-scroll ">
-                  <div>
-                    <h1 className="m-3">
-                      Workouts
-                      <Link href="/workouts">
-                        {/* In Link, wrap icon in span to avoid ref error */}
-                        <span>
-                          <FontAwesomeIcon
-                            icon="chevron-right"
-                            size="sm"
-                            className="chevronicon float-right m-1"
-                          />
-                        </span>
-                      </Link>
-                    </h1>
-                  </div>
+
+              <div className="dash-card col-span-5 row-span-2">
+                <div className="fade-bottom">
+                  <h1 className="m-3">
+                    Workouts
+                    <Link href="/workouts">
+                      <span>
+                        <FontAwesomeIcon
+                          icon="chevron-right"
+                          size="sm"
+                          className="chevronicon float-right m-1"
+                        />
+                      </span>
+                    </Link>
+                  </h1>
 
                   <Suspense fallback={<div>Loading...</div>}>
                     <WorkoutsList />
                   </Suspense>
                 </div>
+              </div>
 
-                {/* Weekly Plans */}
-                <div className="row-span-1 overflow-y-scroll">
+              <div className="dash-card col-span-5 row-span-2">
+                <div>
                   <h1 className="m-3">
                     Weekly Plans
                     <Link href="/plans">
-                      {/* In Link, wrap icon in span to avoid ref error */}
                       <span>
                         <FontAwesomeIcon
                           icon="chevron-right"
@@ -127,88 +118,17 @@ const Home: BlitzPage = () => {
         </div>
 
         {/*  */}
-        {/* <div className="min-h-screen items-center"> */}
-        {/* <div className="flex-1 max-w-4xl mx-auto p-10"> */}
-        <div className="grid grid-cols-5 grid-rows-4 gap-5 grid-flow-row grid-flow-col">
-          <div className="col-start-1 col-span-3 row-start-1 row-span-4">
-            <div className="card-container-parent">
-              <div className="dash-container ml-20 h-full">
-                {/* Current Workout */}
 
-                <h1 className="m-3">
-                  Current Workout
-                  <Link href="/">
-                    {/* In Link, wrap icon in span to avoid ref error */}
-                    <span>
-                      <FontAwesomeIcon
-                        icon="chevron-right"
-                        size="sm"
-                        className="chevronicon float-right m-1"
-                      />
-                    </span>
-                  </Link>
-                </h1>
-                <div className="m-4">No workouts to show ...</div>
-              </div>
-            </div>
-          </div>
-          <div className="row-span-2 col-span-2 mr-20">
-            <div className="dash-container overflow-y-scroll h-full">
-              <div className="">
-                <h1 className="m-3">
-                  Workouts
-                  <Link href="/workouts">
-                    {/* In Link, wrap icon in span to avoid ref error */}
-                    <span>
-                      <FontAwesomeIcon
-                        icon="chevron-right"
-                        size="sm"
-                        className="chevronicon float-right m-1"
-                      />
-                    </span>
-                  </Link>
-                </h1>
-
-                <Suspense fallback={<div>Loading...</div>}>
-                  <WorkoutsList />
-                </Suspense>
-              </div>
-            </div>
-            {/*  </div>
-          <div className="row-span-2 col-span-2 mr-20"> */}
-            <div className="dash-container overflow-y-scroll h-full">
-              <h1 className="m-3">
-                Weekly Plans
-                <Link href="/plans">
-                  {/* In Link, wrap icon in span to avoid ref error */}
-                  <span>
-                    <FontAwesomeIcon
-                      icon="chevron-right"
-                      size="sm"
-                      className="chevronicon float-right m-1"
-                    />
-                  </span>
-                </Link>
-              </h1>
-              <Suspense fallback={<div>Loading...</div>}>
-                <PlansList />
-              </Suspense>
-            </div>
-          </div>
-        </div>
-        {/* </div> */}
-        {/* </div> */}
-
-        {/*  <div className="min-h-screen flex items-center">
+        {/* <div className="min-h-screen flex items-center">
           <div className="flex-1 max-w-4xl mx-auto p-10">
             <div className="grid grid-cols-6 grid-rows-4 gap-4 grid-flow-row grid-flow-col">
-              <div className="col-start-1 col-span-2 row-start-1 row-span-4 bg-white rounded-lg shadow-xl">
+              <div className="col-start-1 col-span-2 row-start-1 row-span-4 card">
                 <div></div>
               </div>
-              <div className="bg-white rounded-lg shadow-xl row-span-2">
+              <div className="card row-span-2">
                 <div></div>
               </div>
-              <div className="bg-white rounded-lg shadow-xl row-span-2">
+              <div className="card row-span-2">
                 <div></div>
               </div>
             </div>
@@ -235,6 +155,18 @@ const Home: BlitzPage = () => {
           background-color: #333;
           border-radius: 20px;
           border: none;
+        }
+
+        .fade-bottom {
+          -webkit-mask-image: -webkit-gradient(
+            linear,
+            left 90%,
+            left-bottom,
+            from(rgba(0, 0, 0, 1)),
+            to(rgba(0, 0, 0, 0))
+          );
+
+          mask-image: linear-gradient(to bottom, white 90%, transparent);
         }
       `}</style>
     </div>
